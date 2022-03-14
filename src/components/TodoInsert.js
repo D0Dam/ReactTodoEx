c13efa1deb4dfb59fd12148fb76a3e104df88011
KimMinJae <smallkdb@gmail.com>
2022-03-15 01:40:58 +0900

    finish todoproject

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInserts }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInserts(value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInserts, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
